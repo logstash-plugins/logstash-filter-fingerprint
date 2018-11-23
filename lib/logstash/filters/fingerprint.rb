5fe864d6067f86024941fb0994e44c675d9ede0a
João Duarte <jsvd@users.noreply.github.com>
2018-11-23 00:14:49 +0000

    make openssl digest objects thread local (#47)

--- lib/logstash/filters/fingerprint.rb
+++ lib/logstash/filters/fingerprint.rb
@@ -100,7 +100,6 @@ class LogStash::Filters::Fingerprint < LogStash::Filters::Base
       # nothing
     else
       class << self; alias_method :fingerprint, :fingerprint_openssl; end
-      @digest = select_digest(@method)
     end
   end
 
@@ -153,19 +152,27 @@ class LogStash::Filters::Fingerprint < LogStash::Filters::Base
   end
 
   def fingerprint_openssl(data)
+    # since OpenSSL::Digest instances aren't thread safe, we must ensure that
+    # each pipeline worker thread gets its own instance.
+    # Also, since a logstash pipeline may contain multiple fingerprint filters
+    # we must include the id in the thread local variable name, so that we can
+    # store multiple digest instances
+    digest_string = "digest-#{id}"
+    Thread.current[digest_string] ||= select_digest(@method)
+    digest = Thread.current[digest_string]
     # in JRuby 1.7.11 outputs as ASCII-8BIT
     if @key.nil?
       if @base64encode
-        @digest.base64digest(data.to_s).force_encoding(Encoding::UTF_8)
+        digest.base64digest(data.to_s).force_encoding(Encoding::UTF_8)
       else
-        @digest.hexdigest(data.to_s).force_encoding(Encoding::UTF_8)
+        digest.hexdigest(data.to_s).force_encoding(Encoding::UTF_8)
       end
     else
       if @base64encode
-        hash = OpenSSL::HMAC.digest(@digest, @key, data.to_s)
+        hash = OpenSSL::HMAC.digest(digest, @key, data.to_s)
         Base64.strict_encode64(hash).force_encoding(Encoding::UTF_8)
       else
-        OpenSSL::HMAC.hexdigest(@digest, @key, data.to_s).force_encoding(Encoding::UTF_8)
+        OpenSSL::HMAC.hexdigest(digest, @key, data.to_s).force_encoding(Encoding::UTF_8)
       end
     end
   end
